1b96503dccfdfd7fdaa9c2ebad053ac5f5f80bdd
Eldar Aidumov <superaydumov@me.com>
2023-04-23 20:39:50 +0300

    Initial Commit

---
+++ SuperApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuperApp
+//
+//  Created by Эльдар Айдумов on 23.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
